e4195ef0b51c03bd17a864f5dfcf3a7f087a3116
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-04-30 13:04:32 -0500

    completed simple_function.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,30 @@
+"use strict"
+
+
+//function to dsiplay my name, favorite movei, and my favorite band
+function favoriteThings(){
+
+    console.log("Italy");
+    console.log("Forrest Gump");
+    console.log("Rex Orange County");
+
+}
+function whyImHere(){
+
+    console.log("I joined Learn to Code to learn new material and explore a new career");
+}
+function favoritePlace (){
+
+    console.log("My favorite place to visit is South Padre Island as it is a peaceful place for me and I love the beach");
+}
+
+
+
+
+
+//calling our functions
+favoriteThings();
+
+whyImHere();
+
+favoritePlace();
